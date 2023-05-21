03839b25dfe15e86e3423c89c60396eca1843c03
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-21 16:27:11 -0400

    Custom pipe

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -48,8 +48,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if(deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not founf`);
+    return;
   }
 
   private handleExceptions(error: any) {
