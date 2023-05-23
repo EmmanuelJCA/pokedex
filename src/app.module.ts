dc3252ce061fbc75ce70b4ebb7a85d3651d68f48
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-22 22:25:44 -0400

    env initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/pokedex'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
